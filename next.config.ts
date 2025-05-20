13886784d8accff7a4d49e8d5082d5ea0346c420
UltraRaptor <sohhongyu@gmail.com>
2025-05-20 16:09:16 +0000

    Added Supabase Upload for Uploading of Images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["https://hlzsmadaanjcpyjghntc.supabase.co"],
+  },
 };
 
 export default nextConfig;
